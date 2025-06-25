8007da3e3b30d79b86a0c8d7fb45e3cf0f9892f3
lg221 <liverboy233@gmail.com>
2025-06-25 07:07:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'SUPUN-MD=JxwkACIb#rJXgszOg4n1qJtgD-ZIjIN_Ljhcp8XY8st9qVdoDjXo' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
-SUDO: process.env.SUDO === undefined ? '94765684096' : process.env.SUDO,
+SUDO: process.env.SUDO === undefined ? '94728132970' : process.env.SUDO,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
 };
