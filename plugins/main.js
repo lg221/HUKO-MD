56198b3ddd80cc988657f232a876a207c71e3d9a
lg221 <liverboy233@gmail.com>
2025-06-25 07:53:42 +0530

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -1,4 +1,4 @@
-const { cmd, commands } = require('../command');
+const { cmd, commands } = require('../lib/command');
 const config = require('../settings');
 const {readEnv} = require('../lib/database');
 const os = require('os');
